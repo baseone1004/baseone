2e49a813d1600a55cad51fd8a86873d38189fe65
baseone1004 <baseone1004@gmail.com>
2026-01-22 19:18:33 +0900

    Update server.py

--- server.py
+++ server.py
@@ -264,3 +264,7 @@ def api_blogger_post():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "5000"))
     app.run(host="0.0.0.0", port=port)
+@app.route("/__routes")
+def __routes():
+    return jsonify(sorted([str(r) for r in app.url_map.iter_rules()]))
+
